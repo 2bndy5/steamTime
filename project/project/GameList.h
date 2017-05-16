dfe849a611f7c7b054542514fbeb98025c0f6a28
Ythmailen <jplatona@ucdavis.edu>
2017-05-16 12:50:59 -0700

    Changed order of private/public
    to make shit work

--- project/project/GameList.h
+++ project/project/GameList.h
@@ -5,17 +5,15 @@
 using std::string;
 
 class GameList {
+private:
+	bool isEmpty();
+	static const int MAX_SIZE = 5;
 public:
 	GameList();
 	~GameList();
 	void print();
 	void addNode(string, unsigned int, unsigned int);
-	static const int MAX_SIZE = 5;
 	ListNode list[MAX_SIZE];
-	
-private:
-	bool isEmpty();
-	
 };
 
 #endif // !Game_H
